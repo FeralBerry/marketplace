59465a9d3368c2f6f6e18f5c0b1bf1573b0d8f9a
FeralHorse <pusiket90@yandex.ru>
2026-02-16 17:03:18 +0300

    the first basic project settings have been created, a database table handler has been added, and a repository has been added for further management of adding, deleting, and editing data from the database

---
+++ java/src/main/java/com/marketplace/repository/UsersRepository.java
@@ -0,0 +1,8 @@
+package com.marketplace.repository;
+
+import com.marketplace.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsersRepository extends JpaRepository<Users,Long> {
+
+}
